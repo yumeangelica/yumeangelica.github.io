5db48ecb9aa8d679eec655b8b98d4aee0bda3360
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2023-10-15 01:30:07 +0300

    add component file

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>&copy; 2020 - {{ year }} yumeangelica.github.io. All Rights Reserved.</footer>
+</template>
+
+<script setup>
+  const year = new Date().getFullYear()
+</script>
+
+<style>
+  footer {
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    color: var(--kawaii-font-color);
+    text-align: center;
+    padding-bottom: 20px;
+    margin-top: 20px;
+  }
+</style>
