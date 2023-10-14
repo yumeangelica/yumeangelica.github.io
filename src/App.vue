b27e5f312c7f3e5900289a3f72d16c7c1526d904
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2023-10-15 01:29:37 +0300

    add App file

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <TheNavBar />
+  </header>
+
+  <main>
+    <div class="container">
+      <RouterView />
+    </div>
+  </main>
+
+  <TheFooter />
+</template>
+
+<script>
+
+  import TheNavBar from 'components/TheNavBar.vue'
+  import TheFooter from 'components/TheFooter.vue'
+
+
+  export default {
+    name: 'App',
+    components: {
+      TheNavBar,
+      TheFooter
+    }
+  }
+
+</script>
+
+
+
+<style>
+  main {
+    flex-grow: 1;
+    min-height: calc(90vh - 50px);
+  }
+</style>
