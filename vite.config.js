67283bfd63e756a1b9b76acae6747e75a4d72ab3
Angelica Järvenpää <74900808+angelicaroselie@users.noreply.github.com>
2023-11-10 19:54:35 +0200

    up

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       "components": fileURLToPath(new URL('./src/components', import.meta.url)),
       "pages": fileURLToPath(new URL('./src/pages', import.meta.url)),
+      "assets": fileURLToPath(new URL('./src/assets', import.meta.url)),
     }
   }
 })
