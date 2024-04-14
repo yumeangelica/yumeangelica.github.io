f9f69b5f299e5ec34effcd70606138332241c101
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-04-14 10:36:04 +0300

    enhance PageProjects styling

--- src/pages/PageProjects.vue
+++ src/pages/PageProjects.vue
@@ -1,10 +1,10 @@
 <template>
-  <h1>Elegant Web Development Creations</h1>
+  <h1 class="text-center">Elegant web development creations</h1>
   <div class="projects-container">
     <TheProjectCard v-for="project in webDevelopmentProjects" :key="project.id" :project="project" :technologies="technologies" />
   </div>
 
-  <h1>Command-line Projects</h1>
+  <h2 class="text-center">Command-line Projects</h2>
   <div class="projects-container">
     <TheProjectCard v-for="project in commandLineProjects" :key="project.id" :project="project" :technologies="technologies" />
   </div>
@@ -13,61 +13,61 @@
 
 
 <script>
-  import TheProjectCard from '../components/TheProjectCard.vue';
-  import { ref } from 'vue';
+import TheProjectCard from '../components/TheProjectCard.vue';
+import { ref } from 'vue';
 
-  export default {
-    data() {
-      return {
-        webDevelopmentProjects: ref([]),
-        commandLineProjects: ref([]),
-        technologies: ref([]),
-        dataURL: '../data.json',
-        dataHandleError: ref(false)
+export default {
+  data() {
+    return {
+      webDevelopmentProjects: ref([]),
+      commandLineProjects: ref([]),
+      technologies: ref([]),
+      dataURL: '../data.json',
+      dataHandleError: ref(false)
+    }
+  },
+  components: {
+    TheProjectCard
+  },
+  async created() { // when site is loaded, fetch data from data.json
+    try {
+      const response = await fetch(this.dataURL);
+      const data = await response.json();
+      if (data) { // if data is fetched successfully, assign it to projects and technologies
+        this.technologies = data.technologies;
+        this.webDevelopmentProjects = data.projects.filter(p => p.type === 'webDevelopment');
+        this.commandLineProjects = data.projects.filter(p => p.type === 'commandLine');
       }
-    },
-    components: {
-      TheProjectCard
-    },
-    async created() { // when site is loaded, fetch data from data.json
-      try {
-        const response = await fetch(this.dataURL);
-        const data = await response.json();
-        if (data) { // if data is fetched successfully, assign it to projects and technologies
-          this.technologies = data.technologies;
-          this.webDevelopmentProjects = data.projects.filter(p => p.type === 'webDevelopment');
-          this.commandLineProjects = data.projects.filter(p => p.type === 'commandLine');
-        }
-      } catch (error) {
-        this.dataHandleError = true;
-        console.log(error.message);
-      }
+    } catch (error) {
+      this.dataHandleError = true;
+      console.log(error.message);
     }
   }
+}
 
 </script>
 
 
 
-<style>
+<style scoped>
+.projects-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  flex-wrap: wrap;
+  align-items: stretch;
+  padding: 20px;
+}
+
+
+/* mobile mode rules */
+@media (max-width: 600px) {
   .projects-container {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: flex-start;
-    /* ensures that cards are aligned to the left */
-    align-items: stretch;
-    /* ensures that all cards are the same height */
-    padding: 20px;
-    gap: 20px;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px;
   }
-
-
-  /* mobile mode rules */
-  @media (max-width: 600px) {
-    .projects-container {
-      flex-direction: column;
-      align-items: center;
-    }
-  }
+}
 </style>
