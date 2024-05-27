e567a7d419dd771134910edcfc7fa1d1f10aa06e
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-05-27 23:36:31 +0300

    update mobile styles

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -3,23 +3,24 @@
 </template>
 
 <script setup>
-const year = new Date().getFullYear()
+  const year = new Date().getFullYear()
 </script>
 
 <style>
-footer {
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  color: var(--color-font-gray);
-  text-align: center;
-  padding: 15px;
-  margin-top: 20px;
-}
-
-@media (max-width: 568px) {
   footer {
-    font-size: 0.60rem;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    color: var(--color-font-gray);
+    text-align: center;
+    padding: 15px;
+    margin-top: 20px;
   }
-}
+
+  @media (max-width: 568px) {
+    footer {
+      font-size: 0.65rem;
+      padding: 15px 5px;
+    }
+  }
 </style>
