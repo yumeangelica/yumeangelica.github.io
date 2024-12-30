f92d70b0a6b824fa17da19b32759291d8219e4a2
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-12-30 21:26:31 +0200

    Add redux and playwright to languages listing

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -26,6 +26,7 @@
         <h3>Web frontend technologies & intuitive store</h3>
         <p class="introduction-highlights-paragraph">
           <img class="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="React Logo" />
+          <img class="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redux/redux-original.svg" alt="Redux Logo" />
           <img class="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original-wordmark.svg" alt="Vue.js Logo" />
           <img class="devicon" src="../assets/img/ionic-logo.svg" alt="Ionic Logo" />
           <img class="devicon" src="../assets/img/pinia-for-vue-logo.webp" alt="Pinia Logo" />
@@ -52,7 +53,10 @@
         <h3>Testing tools</h3>
         <p class="introduction-highlights-paragraph">
           <img class="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" alt="Jest Logo" />
-          <img class="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cypressio/cypressio-original-wordmark.svg" alt="Cypress Logo" />
+          <img class="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cypressio/cypressio-original-wordmark.svg"
+            alt="Cypress Logo" />
+          <img class="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/playwright/playwright-original.svg"
+            alt="Playwright Logo" />
         </p>
       </div>
 
@@ -144,142 +148,142 @@
 </script>
 
 <style scoped>
-  .text-highlight {
-    color: var(--color-extra-dark-bold-lilac);
-    font-weight: bold;
-  }
+.text-highlight {
+  color: var(--color-extra-dark-bold-lilac);
+  font-weight: bold;
+}
+
+.devicon {
+  max-width: 70px;
+  margin-right: 15px;
+  margin-bottom: 15px;
+  padding: 2px;
+  border-radius: 5px;
+}
+
+.contact-icon {
+  max-width: 80px;
+  margin-right: 25px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+}
+
+.contact-image {
+  max-width: 90%;
+  width: 320px;
+}
+
+.contact-icon:hover {
+  transform: scale(1.05);
+  cursor: pointer;
+}
+
+.profilepic {
+  width: 100%;
+  max-width: 300px;
+  margin: 10px 0px 20px 35px;
+}
+
+/* Unordered heartlist */
+.heartlist ul {
+  list-style: none;
+}
+
+.heartlist ul li:before {
+  content: "\2661";
+  color: var(--color-semi-dark-lilac);
+  font-size: 1.1rem;
+  position: absolute;
+  margin-left: -1.1em;
+}
+
+.heartlist li {
+  font-size: 1.1rem;
+  position: relative;
+  padding: 1px 0px 1px 0px;
+}
+
+.introduction-highlights-paragraph {
+  margin-bottom: 20px;
+}
+
+.tooltip-container {
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+  /* Creates a dashed underline */
+  border-bottom: 1px dashed var(--color-semi-dark-lilac);
+  padding-bottom: 2px;
+}
+
+.tooltip-text {
+  font-size: 22px;
+  visibility: hidden;
+  opacity: 0;
+  width: auto;
+  color: var(--color-semi-dark-lilac);
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 10px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+  transition: opacity 0.4s ease, visibility 0.4s ease;
+
+  /* Position the tooltip */
+  position: absolute;
+  z-index: 1;
+  bottom: 0;
+  left: 110%;
+  /* Positions the tooltip to the right of the text */
+  transform: translateY(-50%);
+  white-space: nowrap;
+}
+
+.tooltip-container:hover .tooltip-text {
+  visibility: visible;
+  opacity: 1;
+}
+
+/* mobile mode rules */
+@media (max-width: 568px) {
 
   .devicon {
-    max-width: 70px;
-    margin-right: 15px;
-    margin-bottom: 15px;
-    padding: 2px;
-    border-radius: 5px;
+    max-width: 45px;
+    margin-right: 10px;
   }
 
   .contact-icon {
-    max-width: 80px;
-    margin-right: 25px;
-    margin-bottom: 15px;
-    border-radius: 5px;
+    max-width: 55px;
   }
 
-  .contact-image {
-    max-width: 90%;
-    width: 320px;
-  }
-
-  .contact-icon:hover {
-    transform: scale(1.05);
-    cursor: pointer;
-  }
-
-  .profilepic {
-    width: 100%;
-    max-width: 300px;
-    margin: 10px 0px 20px 35px;
-  }
-
-  /* Unordered heartlist */
-  .heartlist ul {
-    list-style: none;
+  .heartlist li,
+  p {
+    font-size: 0.9rem;
   }
 
   .heartlist ul li:before {
-    content: "\2661";
-    color: var(--color-semi-dark-lilac);
-    font-size: 1.1rem;
-    position: absolute;
-    margin-left: -1.1em;
+    font-size: 0.9rem;
   }
 
-  .heartlist li {
-    font-size: 1.1rem;
-    position: relative;
-    padding: 1px 0px 1px 0px;
-  }
-
-  .introduction-highlights-paragraph {
-    margin-bottom: 20px;
-  }
-
-  .tooltip-container {
-    position: relative;
-    display: inline-block;
-    cursor: pointer;
-    /* Creates a dashed underline */
-    border-bottom: 1px dashed var(--color-semi-dark-lilac);
-    padding-bottom: 2px;
+  .profilepic {
+    max-width: 200px;
+    margin: 10px auto;
+    display: block;
   }
 
   .tooltip-text {
-    font-size: 22px;
+    font-size: 16px;
     visibility: hidden;
     opacity: 0;
-    width: auto;
-    color: var(--color-semi-dark-lilac);
-    text-align: center;
-    border-radius: 6px;
-    padding: 5px 10px;
-    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
-    transition: opacity 0.4s ease, visibility 0.4s ease;
-
-    /* Position the tooltip */
     position: absolute;
-    z-index: 1;
-    bottom: 0;
-    left: 110%;
-    /* Positions the tooltip to the right of the text */
-    transform: translateY(-50%);
-    white-space: nowrap;
+    left: auto;
+    right: -45px;
+    bottom: 25px;
+    transform: translateX(0%);
   }
 
   .tooltip-container:hover .tooltip-text {
     visibility: visible;
     opacity: 1;
   }
-
-  /* mobile mode rules */
-  @media (max-width: 568px) {
-
-    .devicon {
-      max-width: 45px;
-      margin-right: 10px;
-    }
-
-    .contact-icon {
-      max-width: 55px;
-    }
-
-    .heartlist li,
-    p {
-      font-size: 0.9rem;
-    }
-
-    .heartlist ul li:before {
-      font-size: 0.9rem;
-    }
-
-    .profilepic {
-      max-width: 200px;
-      margin: 10px auto;
-      display: block;
-    }
-
-    .tooltip-text {
-      font-size: 16px;
-      visibility: hidden;
-      opacity: 0;
-      position: absolute;
-      left: auto;
-      right: -45px;
-      bottom: 25px;
-      transform: translateX(0%);
-    }
-
-    .tooltip-container:hover .tooltip-text {
-      visibility: visible;
-      opacity: 1;
-    }
-  }
+}
 </style>
