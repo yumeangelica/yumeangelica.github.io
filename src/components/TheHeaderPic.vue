67cc3a51b7fd286003eed48a7b14fc18c5dd8fe3
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2025-03-02 05:46:59 +0200

    Add aria-hidden attribute to header image for improved accessibility

--- src/components/TheHeaderPic.vue
+++ src/components/TheHeaderPic.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="header-pic" role="img" aria-label="Sakura banner for header"></div>
+  <div class="header-pic" role="img" aria-label="Sakura banner for header" aria-hidden="true"></div>
 </template>
 
 
 <style>
-  .header-pic {
-    background-image: url(/assets/img/sakurabanner.jpeg);
-    background-position: center;
-    padding: 25px 20px 5px 20px;
-    text-align: center;
-    height: 100px;
-    opacity: 0.7;
-  }
+.header-pic {
+  background-image: url(/assets/img/sakurabanner.jpeg);
+  background-position: center;
+  padding: 25px 20px 5px 20px;
+  text-align: center;
+  height: 100px;
+  opacity: 0.7;
+}
 </style>
